81f7b57c4dfd306d4dc92d75b888b68a2e486804
Etette <83280337+Etette@users.noreply.github.com>
2026-05-20 11:34:24 +0100

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1398,7 +1398,7 @@ export default function Home() {
                         <line x1="8" x2="8" y1="2" y2="6" />
                         <line x1="3" x2="21" y1="10" y2="10" />
                       </svg>
-                      <span>9th - 11th July 2026</span>
+                      <span>TBA</span>
                     </p>
                   </div>
                 </div>
@@ -1464,7 +1464,7 @@ export default function Home() {
                         <line x1="8" x2="8" y1="2" y2="6" />
                         <line x1="3" x2="21" y1="10" y2="10" />
                       </svg>
-                      <span>8th - 13th July 2026</span>
+                      <span>TBA</span>
                     </p>
                   </div>
                 </div>
